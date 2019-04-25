c6c377ac99294c2d048fb397d7d3dd70247d2514
John Elder <johne4196@gmail.com>
2019-04-25 09:53:37 -0700

    CheckBoxes

---
+++ check.py
@@ -0,0 +1,26 @@
+from tkinter import *
+from PIL import ImageTk,Image
+
+
+root = Tk()
+root.title('Codemy.com Image Viewer')
+root.iconbitmap('c:/gui/codemy.ico')
+root.geometry("400x400")
+
+def show():
+	myLabel = Label(root, text=var.get()).pack()
+
+
+var = StringVar()
+
+c =Checkbutton(root, text="Would you like to SuperSize your order? Check Here!", variable=var, onvalue="SuperSize", offvalue="RegularSize")
+c.deselect()
+c.pack()
+
+
+
+myButton = Button(root, text="Show Selection", command=show).pack()
+
+
+root.mainloop()
+
