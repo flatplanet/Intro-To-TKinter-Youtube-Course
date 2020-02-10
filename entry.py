9e7f845f8b80f758425632db8bb615f3d0ea7f4c
John Elder <john@codemy.com>
2020-02-10 08:11:36 -0800

    entry height

--- entry.py
+++ entry.py
@@ -2,13 +2,14 @@ from tkinter import *
 
 root = Tk()
 
-e = Entry(root, width=50)
+e = Entry(root, width=50, font=('Helvetica', 24))
 e.pack()
 e.insert(0, "Enter Your Name: ")
 
 def myClick():
 	hello = "Hello " + e.get()
 	myLabel = Label(root, text=hello)
+	e.delete(0, 'end')
 	myLabel.pack()
 
 myButton = Button(root, text="Enter Your Stock Quote", command=myClick)
