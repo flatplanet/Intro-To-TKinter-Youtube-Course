169a56bba3a783af50eef1e47e6ab3ee1f923d89
John Elder <john@codemy.com>
2020-03-09 08:47:03 -0700

    menu.py

--- classy.py
+++ classy.py
@@ -10,15 +10,20 @@ class Elder:
 
 	def __init__(self, master):
 		myFrame = Frame(master)
+		myFrame.bind("<Key>", self.key)
 		myFrame.pack()
 
-		self.myButton = Button(master, text="Click Me!", command=clicker)
+		self.myButton = Button(master, text="Click Me!")
+		self.myButton.bind("<Button-1>", self.clicker)
 		self.myButton.pack(pady=20)
 
-	def clicker(self):
-		print("Look at you...you clicked a button!")
+	def clicker(self, event):
+		print("Look at you...you clicked a button! ", event.char)
+
+	def key(self, event):
+		print("You pressed this key: ", char)
+	
 
 e = Elder(root)
-
 
 root.mainloop()
