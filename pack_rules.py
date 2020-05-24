c25c5eef1d907d353392ee13c77c31fcd16d2c1a
John Elder <john@codemy.com>
2020-05-24 06:59:17 -0700

    ignore

---
+++ pack_rules.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+root = Tk()
+root.title('Codemy.com')
+root.iconbitmap('c:/gui/codemy.ico')
+root.geometry("600x400")
+
+def grab():
+	my_label.config(text=my_box.get())
+
+
+my_box = Entry(root, font=("Helvetica", 28))
+my_box.pack(pady=20)
+
+my_button = Button(root, text="Grab Text From Box", command=grab)
+my_button.pack(pady=20)
+
+
+my_label = Label(root, text="")
+my_label.pack(pady=20)
+
+
+root.mainloop()
