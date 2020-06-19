d7c3c45f886a0243e9e6eaa8f4bccf5e9d319593
John Elder <john@codemy.com>
2020-06-19 09:06:22 -0700

    jumble hint

--- shuffle.py
+++ shuffle.py
@@ -13,6 +13,13 @@ my_label = Label(root, text="", font=("Helvetica", 48))
 my_label.pack(pady=20)
 
 def shuffler():
+    # Clear Hint Label 
+    hint_label.config(text='')
+
+    # Clear Hint Count
+    global hint_count
+    hint_count = 0
+
     # Clear Answer Box
     entry_answer.delete(0, END)
 
@@ -47,6 +54,23 @@ def answer():
     else:
         answer_label.config(text="Incorrect!!")
 
+# Create Hint Counter
+global hint_count
+hint_count = 0
+
+# Create Hint Function
+def hint(count):
+    global hint_count
+    hint_count = count
+
+    # Get the length of the chosen word
+    word_length = len(word)
+
+    # Show Hint
+    if count < word_length:
+        hint_label.config(text=f'{hint_label["text"]} {word[count]}')
+        hint_count +=1 
+
 entry_answer = Entry(root, font=("Helvetica", 24))
 entry_answer.pack(pady=20)
 
@@ -54,16 +78,21 @@ entry_answer.pack(pady=20)
 button_frame = Frame(root)
 button_frame.pack(pady=20)
 
-my_button = Button(button_frame, text="Pick Another Word", command=shuffler)
-my_button.grid(row=0, column=0, padx=10)
-
 answer_button = Button(button_frame, text="Answer", command=answer)
-answer_button.grid(row=0, column=1, padx=10)
+answer_button.grid(row=0, column=0, padx=10)
+
+my_button = Button(button_frame, text="Pick Another Word", command=shuffler)
+my_button.grid(row=0, column=1, padx=10)
+
+hint_button = Button(button_frame, text="Hint", command=lambda: hint(hint_count))
+hint_button.grid(row=0, column=2, padx=10)
+
 
 answer_label = Label(root, text='', font=("Helvetica", 18))
 answer_label.pack(pady=20)
 
-
+hint_label = Label(root, text='', font=("Helvetica", 18))
+hint_label.pack(pady=10)
 
 shuffler()
 root.mainloop()
