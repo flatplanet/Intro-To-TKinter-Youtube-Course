680a21014e9be385803b3bacf83e396e8eea5383
John Elder <john@codemy.com>
2020-08-10 08:51:16 -0700

    save text file

---
+++ sample_html.html
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <h1>Hello, world!</h1>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ textpad.py
@@ -0,0 +1,128 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import font
+
+root = Tk()
+root.title('Codemy.com - TextPad!')
+root.iconbitmap('c:/gui/codemy.ico')
+root.geometry("1200x660")
+
+
+# Set variable for open file name
+global open_status_name
+open_status_name = False
+
+# Create New File Function
+def new_file():
+	# Delete previous text
+	my_text.delete("1.0", END)
+	# Update status bars
+	root.title('New File - TextPad!')
+	status_bar.config(text="New File        ")
+
+	global open_status_name
+	open_status_name = False
+
+# Open Files
+def open_file():
+	# Delete previous text
+	my_text.delete("1.0", END)
+
+	# Grab Filename
+	text_file = filedialog.askopenfilename(initialdir="C:/gui/", title="Open File", filetypes=(("Text Files", "*.txt"), ("HTML Files", "*.html"), ("Python Files", "*.py"), ("All Files", "*.*")))
+	
+	# Check to see if there is a file name
+	if text_file:
+		# Make filename global so we can access it later
+		global open_status_name
+		open_status_name = text_file
+
+	# Update Status bars
+	name = text_file
+	status_bar.config(text=f'{name}        ')
+	name = name.replace("C:/gui/", "")
+	root.title(f'{name} - TextPad!')
+
+	# Open the file
+	text_file = open(text_file, 'r')
+	stuff = text_file.read()
+	# Add file to textbox
+	my_text.insert(END, stuff)
+	# Close the opened file
+	text_file.close()
+
+# Save As File
+def save_as_file():
+	text_file = filedialog.asksaveasfilename(defaultextension=".*", initialdir="C:/gui/", title="Save File", filetypes=(("Text Files", "*.txt"), ("HTML Files", "*.html"), ("Python Files", "*.py"), ("All Files", "*.*")))
+	if text_file:
+		# Update Status Bars
+		name = text_file
+		status_bar.config(text=f'Saved: {name}        ')
+		name = name.replace("C:/gui/", "")
+		root.title(f'{name} - TextPad!')
+
+		# Save the file
+		text_file = open(text_file, 'w')
+		text_file.write(my_text.get(1.0, END))
+		# Close the file
+		text_file.close()
+
+# Save File
+def save_file():
+	global open_status_name
+	if open_status_name:
+		# Save the file
+		text_file = open(open_status_name, 'w')
+		text_file.write(my_text.get(1.0, END))
+		# Close the file
+		text_file.close()
+
+		status_bar.config(text=f'Saved: {open_status_name}        ')
+	else:
+		save_as_file()
+
+# Create Main Frame
+my_frame = Frame(root)
+my_frame.pack(pady=5)
+
+# Create our Scrollbar For the Text Box
+text_scroll = Scrollbar(my_frame)
+text_scroll.pack(side=RIGHT, fill=Y)
+
+# Create Text Box
+my_text = Text(my_frame, width=97, height=25, font=("Helvetica", 16), selectbackground="yellow", selectforeground="black", undo=True, yscrollcommand=text_scroll.set)
+my_text.pack()
+
+# Configure our Scrollbar
+text_scroll.config(command=my_text.yview)
+
+# Create Menu
+my_menu = Menu(root)
+root.config(menu=my_menu)
+
+# Add File Menu
+file_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="File", menu=file_menu)
+file_menu.add_command(label="New", command=new_file)
+file_menu.add_command(label="Open", command=open_file)
+file_menu.add_command(label="Save", command=save_file)
+file_menu.add_command(label="Save As", command=save_as_file)
+file_menu.add_separator()
+file_menu.add_command(label="Exit", command=root.quit)
+
+# Add Edit Menu
+edit_menu = Menu(my_menu, tearoff=False)
+my_menu.add_cascade(label="Edit", menu=edit_menu)
+edit_menu.add_command(label="Cut")
+edit_menu.add_command(label="Copy")
+edit_menu.add_command(label="Paste")
+edit_menu.add_command(label="Undo")
+edit_menu.add_command(label="Redo")
+
+# Add Status Bar To Bottom Of App
+status_bar = Label(root, text='Ready        ', anchor=E)
+status_bar.pack(fill=X, side=BOTTOM, ipady=5)
+
+
+
+root.mainloop()
