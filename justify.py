9b9b644a278e094a7c1d878124d89c966e139231
John Elder <johne4196@gmail.com>
2020-10-14 08:03:04 -0700

    keys method

--- justify.py
+++ justify.py
@@ -5,10 +5,12 @@ root.title('Codemy.com - Justify Labels')
 root.iconbitmap('c:/gui/codemy.ico')
 root.geometry("500x550")
 
+
 my_label1 = Label(root, 
 	text="Stuff\nStuff Stuff\nStuff Stuff Stuff",
 	font=("Helvetica", 18),
 	bd=1, relief="sunken")
+my_label1.attributes('-alpha', 0.9)
 my_label1.pack(pady=20, ipady=10, ipadx=10)
 
 
---
+++ keys.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+root = Tk()
+root.title('Codemy.com - Keys Method')
+root.iconbitmap('c:/gui/codemy.ico')
+root.geometry("500x550")
+
+my_label = Label(root, text="My Label", font=("Helvetica", 18))
+my_label.pack(pady=20)
+
+my_entry = Entry(root)
+my_entry.pack()
+
+def something():
+	pass
+my_button = Button(root, text="Click Me!", command=something)
+my_button.pack()
+
+for key in my_button.keys():
+	print(key)
+
+print(my_button['command'])
+
+
+
+
+root.mainloop()
