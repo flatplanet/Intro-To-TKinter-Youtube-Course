33e6741ebdfa89d67a3df032cea281b24268913a
John Elder <john@codemy.com>
2020-12-22 08:30:55 -0800

    todo part 2 cross/uncross

--- todo.py
+++ todo.py
@@ -61,10 +61,28 @@ def add_item():
 	my_entry.delete(0, END)
 
 def cross_off_item():
-	pass
+	# Cross off item
+	my_list.itemconfig(
+		my_list.curselection(),
+		fg="#dedede")
+	# Get rid of selection bar
+	my_list.selection_clear(0, END)
 
 def uncross_item():
-	pass
+	# Cross off item
+	my_list.itemconfig(
+		my_list.curselection(),
+		fg="#464646")
+	# Get rid of selection bar
+	my_list.selection_clear(0, END)
+
+def delete_crossed():
+	count = 0
+	while count < my_list.size():
+		if my_list.itemcget(count, "fg") == "#dedede":
+			my_list.delete(my_list.index(count))
+
+		count += 1
 
 
 
@@ -73,11 +91,13 @@ delete_button = Button(button_frame, text="Delete Item", command=delete_item)
 add_button = Button(button_frame, text="Add Item", command=add_item)
 cross_off_button = Button(button_frame, text="Cross Off Item", command=cross_off_item)
 uncross_button = Button(button_frame, text="Uncross Item", command=uncross_item)
+delete_crossed_button = Button(button_frame, text="Delete Crossed", command=delete_crossed)
 
 delete_button.grid(row=0, column=0)
 add_button.grid(row=0, column=1, padx=20)
 cross_off_button.grid(row=0, column=2)
 uncross_button.grid(row=0, column=3, padx=20)
+delete_crossed_button.grid(row=0, column=4)
 
 root.mainloop()
 
