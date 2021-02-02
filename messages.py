0fbaf69fb3fa1ed0959795df2744a3891a3bb39e
John Elder <johne4196@gmail.com>
2021-02-02 08:09:52 -0800

    message widget

---
+++ messages.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+root = Tk()
+root.title('Codemy.com - Message Widget!')
+root.iconbitmap('c:/gui/codemy.ico')
+root.geometry("400x900")
+
+def change():
+	my_message.config(aspect=200)
+
+# First one
+frame1 = LabelFrame(root, text="Right Justified")
+frame1.pack(pady=20)
+
+my_message = Message(frame1, text="This is some \nlong text that I am typing so that we can look at it, isn't it cool!?",
+	font=("helvetica", 18),
+	aspect=150,
+	justify=RIGHT)
+my_message.pack(pady=10, padx=10)
+
+# Second One
+frame2 = LabelFrame(root, text="Left Justified")
+frame2.pack(pady=20)
+
+my_message2 = Message(frame2, text="This is some \nlong text that I am typing so that we can look at it, isn't it cool!?",
+	font=("helvetica", 18),
+	aspect=100,
+	justify=LEFT)
+my_message2.pack(pady=10, padx=10)
+
+# Third One
+frame3 = LabelFrame(root, text="Center Justified")
+frame3.pack(pady=20)
+
+my_message3 = Message(frame3, text="This is some \nlong text that I am typing so that we can look at it, isn't it cool!?",
+	font=("helvetica", 18),
+	aspect=200,
+	justify=CENTER)
+my_message3.pack(pady=10, padx=10)
+
+# Button
+my_button = Button(root, text="Change Text", command=change)
+my_button.pack(pady=20)
+
+root.mainloop()
