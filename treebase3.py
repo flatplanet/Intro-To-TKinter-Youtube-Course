9a5b2cd32557c7b57c59427bcc4c9697c75f0d24
John Elder <johne4196@gmail.com>
2021-04-19 11:18:53 -0700

    treebase part 2

--- treebase3.py
+++ treebase3.py
@@ -144,47 +144,7 @@ zipcode_label.grid(row=1, column=6, padx=10, pady=10)
 zipcode_entry = Entry(data_frame)
 zipcode_entry.grid(row=1, column=7, padx=10, pady=10)
 
-# Move Row Up
-def up():
-	rows = my_tree.selection()
-	for row in rows:
-		my_tree.move(row, my_tree.parent(row), my_tree.index(row)-1)
-
-# Move Rown Down
-def down():
-	rows = my_tree.selection()
-	for row in reversed(rows):
-		my_tree.move(row, my_tree.parent(row), my_tree.index(row)+1)
-
-# Remove one record
-def remove_one():
-	x = my_tree.selection()[0]
-	my_tree.delete(x)
-
-# Remove Many records
-def remove_many():
-	x = my_tree.selection()
-	for record in x:
-		my_tree.delete(record)
-
-# Remove all records
-def remove_all():
-	for record in my_tree.get_children():
-		my_tree.delete(record)
-
-# Clear entry boxes
-def clear_entries():
-	# Clear entry boxes
-	fn_entry.delete(0, END)
-	ln_entry.delete(0, END)
-	id_entry.delete(0, END)
-	address_entry.delete(0, END)
-	city_entry.delete(0, END)
-	state_entry.delete(0, END)
-	zipcode_entry.delete(0, END)
-
-
-# Select Record
+#SELECT RECORD
 def select_record(e):
 	# Clear entry boxes
 	fn_entry.delete(0, END)
@@ -195,12 +155,12 @@ def select_record(e):
 	state_entry.delete(0, END)
 	zipcode_entry.delete(0, END)
 
-	# Grab record Number
+	# Grab record number
 	selected = my_tree.focus()
 	# Grab record values
 	values = my_tree.item(selected, 'values')
 
-	# outpus to entry boxes
+	# output to entry boxes
 	fn_entry.insert(0, values[0])
 	ln_entry.insert(0, values[1])
 	id_entry.insert(0, values[2])
@@ -209,11 +169,38 @@ def select_record(e):
 	state_entry.insert(0, values[5])
 	zipcode_entry.insert(0, values[6])
 
-# Update record
+# Remove one selected
+def remove_one():
+	x = my_tree.selection()[0]
+	my_tree.delete(x)
+
+# Remove many selected
+def remove_many():
+	x = my_tree.selection()
+	for record in x:
+		my_tree.delete(record)
+
+# Move Row up
+def up():
+	rows = my_tree.selection()
+	for row in rows:
+		my_tree.move(row, my_tree.parent(row), my_tree.index(row)-1)
+
+# Move Row Down
+def down():
+	rows = my_tree.selection()
+	for row in reversed(rows):
+		my_tree.move(row, my_tree.parent(row), my_tree.index(row)+1)
+
+# Remove all records
+def remove_all():
+	for record in my_tree.get_children():
+		my_tree.delete(record)
+
 def update_record():
-	# Grab the record number
+	# Grab record number
 	selected = my_tree.focus()
-	# Update record
+	# Save new data
 	my_tree.item(selected, text="", values=(fn_entry.get(), ln_entry.get(), id_entry.get(), address_entry.get(), city_entry.get(), state_entry.get(), zipcode_entry.get(),))
 
 	# Clear entry boxes
@@ -226,6 +213,16 @@ def update_record():
 	zipcode_entry.delete(0, END)
 
 
+# Clear screen
+def clear_entries():
+	# Clear entry boxes
+	fn_entry.delete(0, END)
+	ln_entry.delete(0, END)
+	id_entry.delete(0, END)
+	address_entry.delete(0, END)
+	city_entry.delete(0, END)
+	state_entry.delete(0, END)
+	zipcode_entry.delete(0, END)
 
 # Add Buttons
 button_frame = LabelFrame(root, text="Commands")
@@ -252,12 +249,13 @@ move_up_button.grid(row=0, column=5, padx=10, pady=10)
 move_down_button = Button(button_frame, text="Move Down", command=down)
 move_down_button.grid(row=0, column=6, padx=10, pady=10)
 
-select_record_button = Button(button_frame, text="Clear Entry Boxes", command=clear_entries)
+#select_record_button = Button(button_frame, text="Select Record", command = lambda: select_record(1))
+select_record_button = Button(button_frame, text="Clear Entries", command=clear_entries)
 select_record_button.grid(row=0, column=7, padx=10, pady=10)
 
-# Bind the treeview
-my_tree.bind("<ButtonRelease-1>", select_record)
 
+# Bind the treeview 
+my_tree.bind("<ButtonRelease-1>", select_record)
 
 
 root.mainloop()
