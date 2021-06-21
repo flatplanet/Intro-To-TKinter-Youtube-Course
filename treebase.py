c2ca687c253c393084a5ec6818701446272c7d8e
John Elder <johne4196@gmail.com>
2021-06-21 16:42:47 -0700

    search database

--- treebase.py
+++ treebase.py
@@ -10,6 +10,110 @@ root.title('Codemy.com - TreeBase')
 root.iconbitmap('c:/gui/codemy.ico')
 root.geometry("1000x550")
 
+def query_database():
+	# Clear the Treeview
+	for record in my_tree.get_children():
+		my_tree.delete(record)
+		
+	# Create a database or connect to one that exists
+	conn = sqlite3.connect('tree_crm.db')
+
+	# Create a cursor instance
+	c = conn.cursor()
+
+	c.execute("SELECT rowid, * FROM customers")
+	records = c.fetchall()
+	
+	# Add our data to the screen
+	global count
+	count = 0
+	
+	#for record in records:
+	#	print(record)
+
+
+	for record in records:
+		if count % 2 == 0:
+			my_tree.insert(parent='', index='end', iid=count, text='', values=(record[1], record[2], record[0], record[4], record[5], record[6], record[7]), tags=('evenrow',))
+		else:
+			my_tree.insert(parent='', index='end', iid=count, text='', values=(record[1], record[2], record[0], record[4], record[5], record[6], record[7]), tags=('oddrow',))
+		# increment counter
+		count += 1
+
+
+	# Commit changes
+	conn.commit()
+
+	# Close our connection
+	conn.close()
+
+
+
+def search_records():
+	lookup_record = search_entry.get()
+	# close the search box
+	search.destroy()
+	
+	# Clear the Treeview
+	for record in my_tree.get_children():
+		my_tree.delete(record)
+	
+	# Create a database or connect to one that exists
+	conn = sqlite3.connect('tree_crm.db')
+
+	# Create a cursor instance
+	c = conn.cursor()
+
+	c.execute("SELECT rowid, * FROM customers WHERE last_name like ?", (lookup_record,))
+	records = c.fetchall()
+	
+	# Add our data to the screen
+	global count
+	count = 0
+	
+	#for record in records:
+	#	print(record)
+
+
+	for record in records:
+		if count % 2 == 0:
+			my_tree.insert(parent='', index='end', iid=count, text='', values=(record[1], record[2], record[0], record[4], record[5], record[6], record[7]), tags=('evenrow',))
+		else:
+			my_tree.insert(parent='', index='end', iid=count, text='', values=(record[1], record[2], record[0], record[4], record[5], record[6], record[7]), tags=('oddrow',))
+		# increment counter
+		count += 1
+
+
+	# Commit changes
+	conn.commit()
+
+	# Close our connection
+	conn.close()
+
+
+
+def lookup_records():
+	global search_entry, search
+
+	search = Toplevel(root)
+	search.title("Lookup Records")
+	search.geometry("400x200")
+	search.iconbitmap('c:/gui/codemy.ico')
+
+	# Create label frame
+	search_frame = LabelFrame(search, text="Last Name")
+	search_frame.pack(padx=10, pady=10)
+
+	# Add entry box
+	search_entry = Entry(search_frame, font=("Helvetica", 18))
+	search_entry.pack(pady=20, padx=20)
+
+	# Add button
+	search_button = Button(search, text="Search Records", command=search_records)
+	search_button.pack(padx=20, pady=20)
+
+
+
 def primary_color():
 	# Pick Color
 	primary_color = colorchooser.askcolor()[1]
@@ -55,7 +159,13 @@ option_menu.add_command(label="Highlight Color", command=highlight_color)
 option_menu.add_separator()
 option_menu.add_command(label="Exit", command=root.quit)
 
-
+#Search Menu
+search_menu = Menu(my_menu, tearoff=0)
+my_menu.add_cascade(label="Search", menu=search_menu)
+# Drop down menu
+search_menu.add_command(label="Search", command=lookup_records)
+search_menu.add_separator()
+search_menu.add_command(label="Reset", command=query_database)
 
 # Add Fake Data
 '''
@@ -123,39 +233,6 @@ conn.commit()
 
 # Close our connection
 conn.close()
-
-def query_database():
-	# Create a database or connect to one that exists
-	conn = sqlite3.connect('tree_crm.db')
-
-	# Create a cursor instance
-	c = conn.cursor()
-
-	c.execute("SELECT rowid, * FROM customers")
-	records = c.fetchall()
-	
-	# Add our data to the screen
-	global count
-	count = 0
-	
-	#for record in records:
-	#	print(record)
-
-
-	for record in records:
-		if count % 2 == 0:
-			my_tree.insert(parent='', index='end', iid=count, text='', values=(record[1], record[2], record[0], record[4], record[5], record[6], record[7]), tags=('evenrow',))
-		else:
-			my_tree.insert(parent='', index='end', iid=count, text='', values=(record[1], record[2], record[0], record[4], record[5], record[6], record[7]), tags=('oddrow',))
-		# increment counter
-		count += 1
-
-
-	# Commit changes
-	conn.commit()
-
-	# Close our connection
-	conn.close()
 
 
 
